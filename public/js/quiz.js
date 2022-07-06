5b53a0dc23e15734e14bfafb4aad9ccc10fa71be
Joseph <joseph@josephchan.me>
2022-07-06 14:43:11 -0400

    move static files to public

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -54,35 +54,6 @@ function doneLoading() {
     console.log("done loading")
 }
 
-
-
-// let questions = [
-//     {
-//         question: `Provide community education on _______`,
-//         choice1: `the origin and nature of the pandemic`,
-//         choice2: `the origin and nature of the China flu`,
-//         choice3: `how to achieve herd immunity`,
-//         choice4: `how to minimize the pandemic's mortality rate`,
-//         answer: 1
-//     },
-//     {
-//         question: `According to the recommendations, what should we help the community address?`,
-//         choice1: `Their waning food supplies`,
-//         choice2: `The impacts of social media on mental health`,
-//         choice3: `Their fear and stigma about Asians during Covid-19`,
-//         choice4: `B and C`,
-//         answer: 3
-//     },
-//     {
-//         question: `What should be challenged?`,
-//         choice1: `Oppressive information about the pandemic you hear about from friends and family members`,
-//         choice2: `All information about the pandemic that contradicts whatever has recently been posted on Facebook`,
-//         choice3: `All information about the pandemic, regardless of where it comes from`,
-//         choice4: `False and/or oppressive information about the pandemic, especially coming from public and authority figures`,
-//         answer: 4
-//     }
-// ];
-
 fetchSettingsGSheets();
 
 let questions = []
--- server.js
+++ server.js
@@ -20,57 +20,57 @@ let connectionString = process.env.MONGO_URI;
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then(client => {
         console.log('connected to db');
-        const db = client.db('tarot-cards');
-        const cards = db.collection('cards');
+        const db = client.db('questions');
+        const questions = db.collection('antiasian_racism');
 
         app.get('/', function (req, res) {
             res.sendFile(__dirname + '/index.html');
         })
-        app.get('/edit-cards', function (req, res) {
-            db.collection('cards').find().toArray()
-            .then(results => {
-                res.render('edit-cards.ejs', {cards: results})
-            })
-            .catch(error => console.error(error))
-        })
-        app.get('/api', function (req, res) {
-            db.collection('cards').find().toArray()
-            .then(result => {
-                res.json(result)
-            })
-            .catch(error=> console.error(error))
-        })
-        app.get('/api/random-card', function (req, res) {
-            db.collection('cards').find().toArray()
-            .then(result => {
-                let rand = Math.floor(Math.random() * result.length);
-                let card = result[rand]
-                res.json(card)
-            })
-        })
+        // app.get('/edit-cards', function (req, res) {
+        //     db.collection('cards').find().toArray()
+        //     .then(results => {
+        //         res.render('edit-cards.ejs', {cards: results})
+        //     })
+        //     .catch(error => console.error(error))
+        // })
+        // app.get('/api', function (req, res) {
+        //     db.collection('cards').find().toArray()
+        //     .then(result => {
+        //         res.json(result)
+        //     })
+        //     .catch(error=> console.error(error))
+        // })
+        // app.get('/api/random-card', function (req, res) {
+        //     db.collection('cards').find().toArray()
+        //     .then(result => {
+        //         let rand = Math.floor(Math.random() * result.length);
+        //         let card = result[rand]
+        //         res.json(card)
+        //     })
+        // })
 
-        app.post('/edit-cards', (req, res) => {
-            cards.insertOne(req.body)
-                .then(result => {
-                    res.redirect('/edit-cards')
-                })
-                .catch(error => console.error(error));
-        })
+        // app.post('/edit-cards', (req, res) => {
+        //     cards.insertOne(req.body)
+        //         .then(result => {
+        //             res.redirect('/edit-cards')
+        //         })
+        //         .catch(error => console.error(error));
+        // })
         
-        app.put('/edit-cards', (req, res) => {
-            console.log(req.body)
-        })
+        // app.put('/edit-cards', (req, res) => {
+        //     console.log(req.body)
+        // })
 
-        app.delete('/edit-cards', (req, res) => {
-            cards.deleteOne(
-                { _id: new mongodb.ObjectId(req.body._id) }
-            )
-            .then(result => {
-                console.log(`ObjectId("${req.body._id}")`)
-                res.json(`Deleted card id ${req.body._id}`)
-            })
-            .catch(error => console.error(error))
-        })
+        // app.delete('/edit-cards', (req, res) => {
+        //     cards.deleteOne(
+        //         { _id: new mongodb.ObjectId(req.body._id) }
+        //     )
+        //     .then(result => {
+        //         console.log(`ObjectId("${req.body._id}")`)
+        //         res.json(`Deleted card id ${req.body._id}`)
+        //     })
+        //     .catch(error => console.error(error))
+        // })
         
 
         app.listen(process.env.PORT || PORT, function () {
